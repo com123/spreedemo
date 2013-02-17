c094e458e637ea4b9d228ae6ced2870349a0003d
mahi <mahinder124@gmail.com>
2013-02-17 14:01:16 +0530

    update

--- app/controllers/custom_controller.rb
+++ app/controllers/custom_controller.rb
@@ -4,13 +4,16 @@ class CustomController < ApplicationController
  def assign_persons
 
 order = Spree::Order.find_by_id(params[:order_id])
+user = Spree::User.find_by_id(params[:id])
 if params[:name] = "partner"
    respond_to do|format|
     if order.update_attributes(:partner_id => params[:id].blank? ? nil : params[:id])
 	order.products.collect{|p| p.partner_id = params[:id].blank? ? nil : params[:id] 
  	p.save
 	 }
-	
+	if user 
+	  Spree::OrderMailer.assign_mail(order,user)	 		
+	end
        format.js { render :json => {:valid => true} }
     else
        format.js { render :json => {:valid => false} }
@@ -21,7 +24,10 @@ respond_to do|format|
     if order.update_attributes(:salerepresentative_id => params[:id].blank? ? nil : params[:id] )
 	order.products.collect{|p| p.salerepresentative_id = params[:id].blank? ? nil : params[:id] 
  	p.save
-	 }	
+	 }
+       if user 
+	  Spree::OrderMailer.assign_mail(order,user)	 		
+	end	
        format.js { render :json => {:valid => true} }
     else
        format.js { render :json => {:valid => false} }
