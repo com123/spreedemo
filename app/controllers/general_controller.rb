ec450985c818d95963536afc163dbb0bac9d9cc2
mahi <mahi@mahi-System-Product-Name.(none)>
2013-03-19 08:07:42 +0530

    update

---
+++ app/controllers/general_controller.rb
@@ -0,0 +1,24 @@
+class GeneralController < ApplicationController
+
+def about_us
+render :layout => true
+
+end
+
+
+
+def contact_us
+
+end
+
+
+def services
+
+end
+
+
+def private_polices
+
+end
+
+end
